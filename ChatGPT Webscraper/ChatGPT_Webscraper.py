356adb332870806e04f0b57de5891f15ad2efdec
drop0ne <core_fire@hotmail.com>
2023-01-02 21:33:57 -0600

    upgraded pytube to pytube3

--- ChatGPT Webscraper/ChatGPT_Webscraper.py
+++ ChatGPT Webscraper/ChatGPT_Webscraper.py
@@ -1,15 +1,27 @@
-import pytube
+import pytube3
+
+def progress_function(stream, chunk, file_handle, bytes_remaining):
+    # Calculate the percentage of the file that has been downloaded
+    percent = (100 * (file_size - bytes_remaining)) / file_size
+
+    # Print the progress bar
+    print('\r[{0}{1}] {2:.0f}%'.format('#' * int(percent), ' ' * (100 - int(percent)), percent), end='')
 
 url = input("Enter the YouTube video URL: ")
 
 # Create a YouTube object using the URL
-yt = pytube.YouTube(url)
+yt = pytube3.YouTube(url)
 
-# Get the first video stream available
-stream = yt.streams.first()
+# Get the highest quality video stream available with a resolution of 1080p or less
+stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
+# Get the size of the file in bytes
+file_size = stream.filesize
+
+# Set the progress function
+stream.on_progress(progress_function)
 
 # Download the video to the current working directory
 stream.download()
 
-print("Video downloaded successfully!")
-
+print("\nVideo downloaded successfully!")
