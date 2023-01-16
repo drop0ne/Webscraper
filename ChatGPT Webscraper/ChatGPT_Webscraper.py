3f5c42bbfe59fa0eacb5f66c4f1d46c0d967c9f4
drop0ne <core_fire@hotmail.com>
2023-01-15 19:40:31 -0600

    refactored

--- ChatGPT Webscraper/ChatGPT_Webscraper.py
+++ ChatGPT Webscraper/ChatGPT_Webscraper.py
@@ -1,5 +1,6 @@
-import pytube3
+import requests
 import os
+import pytube3
 
 def progress_function(stream, chunk, file_handle, bytes_remaining):
     # Calculate the percentage of the file that has been downloaded
@@ -7,25 +8,21 @@ def progress_function(stream, chunk, file_handle, bytes_remaining):
 
     # Print the progress bar
     print('\r[{0}{1}] {2:.0f}%'.format('#' * int(percent), ' ' * (100 - int(percent)), percent), end='')
-
-while True:
-    url = input("Enter the YouTube video URL (or enter 'q' to quit): ")
-    if url.lower() == 'q':
-        break
-
+    
+def download_youtube_video(url, download_path):
     try:
         # Create a YouTube object using the URL
         yt = pytube3.YouTube(url)
     except pytube3.exceptions.RegexMatchError:
         print("Invalid URL, please try again.")
-        continue
+        return
 
     try:
         # Get the highest quality video stream available with a resolution of 1440p or less
         streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().filter(lambda stream: stream.resolution in ['1440p', '1080p', '720p', '480p', '360p', '240p'])
     except pytube3.exceptions.VideoUnavailableError:
         print("Video not available, please try another video.")
-        continue
+        return
 
     # Print available video qualities
     for i, stream in enumerate(streams):
@@ -40,14 +37,6 @@ while True:
     # Get the size of the file in bytes
     file_size = stream.filesize
 
-    # Ask the user for the download location
-    download_path = input("Enter the download location (leave blank for current directory): ")
-
-    if download_path:
-        os.makedirs(download_path, exist_ok=True)
-    else:
-        download_path = os.getcwd()
-
     # Set the progress function
     stream.on_progress(progress_function)
 
@@ -56,8 +45,38 @@ while True:
 
     print("\nVideo downloaded successfully!")
 
-    more = input("Do you want to download another video? (y/n): ")
-    if more.lower() == 'n':
-        break
+def download_pornhub_video(url, download_path):
+    try:
+        response = requests.get(url)
+        open(download_path+"/video.mp4", "wb").write(response.content)
+        print("\nVideo downloaded successfully!")
+    except requests.exceptions.RequestException as e:
+        print(f'Error occured: {e}')
+
+while True:
+    website = input("Which website do you want to download a video from? (Youtube/Pornhub) ")
+    if website.lower() == "youtube":
+        url = input("Please enter the URL of the Youtube video: ")
+        download_path = input("Enter the download location (leave blank for current directory): ")
+        if download_path:
+            os.makedirs(download_path, exist_ok=True)
+        else:
+            download_path = os.getcwd()
+        download_youtube_video(url, download_path)
+    elif website.lower() == "pornhub":
+        url = input("Please enter the URL of the Pornhub video: ")
+        download_path = input("Enter the download location (leave blank for current directory): ")
+        if not download_path:
+            download_path = os.getcwd()
+        os.makedirs(download_path, exist_ok=True)
+        download_pornhub_video(url, download_path)
+    else:
+        print("Invalid website, please try again.")
+        choice = input("Do you want to download another video? (yes/no)")
+        if choice.lower() == "no":
+            break
 
 print("Exiting program...")
+
+
+
